acb2b964d8888740074e5adce85a789e75e7aae6
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-09-15 12:31:07 -0600

    Created monster 'Endon' in the cotroller and the monster 'basic'.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,15 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
+
 public class MonsterController
 {
 
+	public void start()
+	{
+		MarshmallowMonster basic = new MarshmallowMonster();
+		System.out.println(basic);
+		
+	}
 }
