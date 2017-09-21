7128f9be1a5c239bacfd0607c856959afafd294d
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-09-21 12:38:22 -0600

    Added Helper method but code only shows "Exception" in console.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -5,46 +5,54 @@ import java.util.Scanner;
 
 import monster.view.MonsterDisplay;
 
-
 public class MonsterController
 {
 	private MonsterDisplay popup;
-	
+
 	public void start()
 	{
 		MarshmallowMonster basic = new MarshmallowMonster();
-//		System.out.println(basic);
+		// System.out.println(basic);
 		popup.displayText(basic.toString());
 		MarshmallowMonster endon = new MarshmallowMonster("Endon", 3, 2, .23, true);
-//		System.out.println(endon); 
+		// System.out.println(endon);
 		popup.displayText(endon.toString());
-//		System.out.println("I am hungry, I'm go eat one of Endon's arms");
+		// System.out.println("I am hungry, I'm go eat one of Endon's arms");
 		popup.displayText("I am hungry, I'm go eat one of Endon's arms");
 		endon.setArmCount(endon.getArmCount() - 1);
-//		System.out.println(endon);
+		// System.out.println(endon);
 		popup.displayText(endon.toString());
-		
+
 		interactWithMonster(endon);
-		
+
 	}
-	
+
 	private void interactWithMonster(MarshmallowMonster currentMonster)
 	{
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, type how many you want to eat");
-		int consumed = myScanner.nextInt();
+		// System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, type
+		// how many you want to eat");
+		int consumed = 0;
+		String response = popup.getResponse(currentMonster.getName() + " wants to know how many eyes you want to eat, type how many you want to eat");
+		if (isValidInteger(response))
+		{
+			consumed = Integer.parseInt(response);
+		}
+		consumed = Integer.parseInt(response);
+
+		// int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 		System.out.println(currentMonster);
-		
+
 		System.out.println("How many arms do you want to eat? I have " + currentMonster.getArmCount() + " right now!");
-		//consumed = myScanner.nextInt();
+		// consumed = myScanner.nextInt();
 		int armEat = myScanner.nextInt();
-		
-		if(armEat == 0)
+
+		if (armEat == 0)
 		{
 			System.out.println("Not hungry? Why? Don't you want to eat me?Aw well, looks like you get to starve.");
 		}
-		else if(armEat < 0)
+		else if (armEat < 0)
 		{
 			System.out.println("I guess physics and math is hard for you, huh?");
 		}
@@ -56,24 +64,24 @@ public class MonsterController
 		{
 			System.out.println("Ok, now I have " + currentMonster.getArmCount());
 		}
-		
+
 		System.out.println("I have " + currentMonster.getTentacleAmount() + " right now, how many do you want?");
-		//consumed = myScanner.nextInt();
+		// consumed = myScanner.nextInt();
 		double tentacleEat = myScanner.nextDouble();
-		
-		if(tentacleEat == 0)
+
+		if (tentacleEat == 0)
 		{
 			System.out.println("Fine, no delicious tentacles for you.");
 		}
-		else if(tentacleEat < 0)
+		else if (tentacleEat < 0)
 		{
 			System.out.println("You can't eat negative tentacles silly");
 		}
-		else if(tentacleEat == currentMonster.getTentacleAmount())
+		else if (tentacleEat == currentMonster.getTentacleAmount())
 		{
 			System.out.println("YOU ATE ALL OF MY TENTACLES!!!");
 		}
-		else if(tentacleEat > currentMonster.getTentacleAmount())
+		else if (tentacleEat > currentMonster.getTentacleAmount())
 		{
 			System.out.println("If I had that many tentacles I would've told you");
 		}
@@ -82,11 +90,29 @@ public class MonsterController
 			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - tentacleEat);
 			System.out.println("Aight, because you ate " + tentacleEat + " I now have " + currentMonster.getTentacleAmount() + " tentacles!");
 		}
-		
+
 		popup.displayText("Hi there ready to play???");
 		String answer = popup.getResponse("What is the air spped of a cocnut laden swallow?");
 		System.out.println(answer);
-		
+
 		myScanner.close();
 	}
-}	
+
+	// Helper Method
+	private boolean isValidInteger(String sample)
+	{
+		boolean valid = false;
+
+		try
+		{
+			Integer.parseInt(sample);
+			valid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			popup.displayText("ou need to input an int, " + sample + " is not valid.");
+		}
+		return valid;
+	}
+
+}
